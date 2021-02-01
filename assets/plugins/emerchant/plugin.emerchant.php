c80e13f58554a17470a5928c2cb4b0ea4c8d55b3
liber87 <bd87@bk.ru>
2021-02-01 08:03:30 +0300

    rename shk -> em

--- assets/plugins/emerchant/plugin.emerchant.php
+++ assets/plugins/emerchant/plugin.emerchant.php
@@ -12,51 +12,51 @@
 			unset($_POST['cart_name']);
 		}		
 
-		$shk = new emerchant($modx, $params);
+		$em = new emerchant($modx, $params);
 		header("HTTP/1.1 200 OK");
 
 
 		//Добавляем
 		if (isset($_GET['add'])){
-			$position = $shk->makePosition($_POST);
-			$shk->addToCart($position);
-			$shk->saveCart('add');
+			$position = $em->makePosition($_POST);
+			$em->addToCart($position);
+			$em->saveCart('add');
 		}
 		
 
 		//Удаляем
 		if ((isset($_GET['del'])) && (isset($_GET['hash']))){			
-			$shk->removeFromCart($_GET['hash']);
-			$shk->saveCart('remove');	
+			$em->removeFromCart($_GET['hash']);
+			$em->saveCart('remove');	
 		}
 
 		//Изменяем количество
 		if ((isset($_GET['recount'])) && (isset($_GET['hash'])) && ($_GET['count'])){
-			$shk->recountPositionCart($_GET['hash'],$_GET['count']);
-			$shk->saveCart('recount');
+			$em->recountPositionCart($_GET['hash'],$_GET['count']);
+			$em->saveCart('recount');
 		}		
 
 		//Очищаем корзину
 		if (isset($_GET['clearCart'])) {
-			$shk->clearCart();
+			$em->clearCart();
 		}	
 
 		//Сохраняем корзину
 		if (isset($_GET['saveorder'])){	
 
 			//Запихиваем данные в админку
-			$oid = $shk->saveOrder($_POST);								
+			$oid = $em->saveOrder($_POST);								
 
 			//Отправляем письмо админу 
-			$shk->sendLetter($oid,$shk->config['emailTo'],'admin');
+			$em->sendLetter($oid,$em->config['emailTo'],'admin');
 			
 			//Отправляем письмо пользователю 
-			if ($_POST['email']) $shk->sendLetter($oid,$_POST['email'],'user');
+			if ($_POST['email']) $em->sendLetter($oid,$_POST['email'],'user');
 
 			//Обнуляем <s>Путина</s> корзину
-			$shk->clearCart();		
+			$em->clearCart();		
 		}		
-		$shk->yankeeGoHome();
+		$em->yankeeGoHome();
 	}
 
 	//MODULE		
@@ -71,15 +71,15 @@
 			$data = array();
 			$params['orderId'] = $data['orderID'] = $oid;		
 			$shk = new emerchant($modx, $params);
-			$data['form'] = $shk->makeForm($params['orderId']);
-			$data['cart'] = $shk->makeCart($shk->config['module.order.row.tpl'],$shk->config['module.order.owner.tpl']);	
-			echo $shk->tpl->parseChunk($shk->config['module.order.info.tpl'],$data);
+			$data['form'] = $em->makeForm($params['orderId']);
+			$data['cart'] = $em->makeCart($em->config['module.order.row.tpl'],$em->config['module.order.owner.tpl']);	
+			echo $em->tpl->parseChunk($em->config['module.order.info.tpl'],$data);
 			exit();		
 		}
 
 		if (isset($_GET['deleteOrder'])){
-			$modx->db->query('Delete from '.$shk->ordertable.' where id='.$oid);
-			echo $shk->ordertable;
+			$modx->db->query('Delete from '.$em->ordertable.' where id='.$oid);
+			echo $em->ordertable;
 		}
 
 		if (isset($_GET['editOrder'])){
@@ -91,15 +91,15 @@
 		}
 
 		if (isset($_GET['updateOrder'])){
-			$info = json_decode($modx->db->getValue('Select `form` from '.$shk->ordertable.' where id = '.$oid),true);			
+			$info = json_decode($modx->db->getValue('Select `form` from '.$em->ordertable.' where id = '.$oid),true);			
 			foreach($info as $key => $val){
 				if ($_POST[$key]) $info[$key]=$modx->db->escape($_POST[$key]);									
 			}			
 			$modx->db->update(array('form'=>json_encode($info,JSON_UNESCAPED_UNICODE),'status'=>$_POST['status']),
-			$shk->ordertable,'id='.$oid);
+			$em->ordertable,'id='.$oid);
 		}		
 
-		$shk->yankeeGoHome();
+		$em->yankeeGoHome();
 	}
 
 	if ($modx->event->name=='OnWebPagePrerender')
